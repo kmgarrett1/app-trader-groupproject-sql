9e89cc5cfec93af36044477d418a60cbed50ca69
Abigail Aspergit config --global user.email abigailasper@gmail.comgit branchcd <abigailasper@MacBook-Pro.attlocal.net>
2022-02-22 22:02:45 -0600

    initial table with duplicate names and casts done

--- scripts/app_trader_sql.sql
+++ scripts/app_trader_sql.sql
@@ -1,2 +1,17 @@
-select *
-from play_store_apps
+select 
+	a.name, 
+	a.price, 
+	CAST(a.review_count AS numeric) AS review_count_num, 
+	CAST(a.rating AS numeric) AS rating_num, 
+	CAST(REPLACE(a.content_rating, '+','') AS numeric) AS content_rating_num, 
+	a.primary_genre, 
+	CAST(REPLACE(REPLACE(p.install_count, '+',''), ',','') AS numeric) AS install_count_num,
+	p.type
+from app_store_apps as a
+inner join play_store_apps as p
+ON a.name = p.name
+ORDER BY 
+	rating_num DESC,
+	install_count_num DESC,
+	review_count_num DESC,  
+	a.price;
