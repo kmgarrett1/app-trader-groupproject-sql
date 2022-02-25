e1ac55f9daf9fa37b3eb76596ae87cb392bad65d
Cuiweiyi <pcunningham1489@yahoo.com>
2022-02-24 20:36:45 -0600

    what we went with

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -1,4 +1,3 @@
-
 -- 3. Deliverables
 -- a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.
 
@@ -7,49 +6,93 @@
 -- c. Prepare a 5-10 minute presentation for the leadership team of App Trader to inform them of your recommendations.
 
 -- All analysis work must be done in PostgreSQL, however you may export query results if you want to create charts in Excel for your presentations.
-SELECT  ps.name, ps.genres AS dis_gen, AVG(ps.rating) AS avg_rate, sum(ps.review_count) AS num_reviews
-FROM play_store_apps AS ps
-FULL JOIN app_store_apps AS aa
-ON ps.genres = aa.primary_genre
-WHERE ps.rating IS NOT NULL
-AND ps.review_count > 20
-GROUP BY ps.review_count, genres, ps.name
-ORDER BY avg_rate DESC, num_reviews DESC
-LIMIT 20;
--- This line is to deliniate between codes. Below is Rudy's
-select p.name, p.price, p.rating, a.size_bytes, p.install_count
-from play_store_apps as p
-left join app_store_apps as a
-on p.name = a.name
-order by install_count DESC;
--- This line is to deliniate between codes.
-SELECT DISTINCT p.name, p.price, REPLACE(p.install_count, '+', ' ') AS installed
-FROM play_store_apps as p
-LEFT JOIN app_store_apps AS a
-ON p.name = a.name
-ORDER BY installed DESC, price
-LIMIT 25;
--- This line is to deliniate between codes.
-select name, category, rating, review_count, type, price, content_rating, primary_genre
-from app_store_apps as a
-UNION ALL 
-SELECT name, category, rating, review_count, install_count, type, price, content_rating, genre
-FROM play_store_apps as p
-ON a.name = p.name
-;
--- abigail's code
 select 
-	a.name, 
-	a.price, 
-	CAST(a.review_count AS int), 
-	a.rating, 
-	REPLACE(a.content_rating, '+','') AS content_rating, 
+	DISTINCT a.name, 
+	CAST(a.price AS money) AS ap,
+	CAST(p.price AS money) AS pp,
+	COUNT(a.review_count) AS a_review_count_num, 
+	COUNT(p.review_count) AS p_review_count_num,
+	AVG(a.rating) + AVG(p.rating)/(AVG(a.rating) + AVG(p.rating)) AS rating_num,
+	SUM(CAST(REPLACE(a.content_rating, '+','') AS numeric)) AS content_rating_num, 
 	a.primary_genre, 
-	REPLACE(CAST(p.install_count AS int), '+','') AS install_count, 
+	SUM(CAST(REPLACE(REPLACE(p.install_count, '+',''), ',','') AS numeric)) AS install_count_num,
 	p.type
 from app_store_apps as a
 inner join play_store_apps as p
 ON a.name = p.name
-ORDER BY a.review_count DESC, a.rating DESC, a.price, install_count DESC;
+WHERE a.price < '1' AND p.price < '1'
+GROUP BY primary_genre, a.name, a.price, p.price, p.rating, p.type, a.rating, p.rating
+ORDER BY 
+	rating_num DESC,
+	install_count_num DESC,
+	primary_genre DESC, a.name, ap;
+-- This line shall not be crossed.
+select 
+	a.name, 
+	CAST(a.price AS money),
+	CAST(a.rating AS numeric) AS a_rating,
+	CAST(p.rating AS numeric) AS p_rating,
+	CAST(REPLACE(a.content_rating, '+','') AS numeric) AS content_rating_num,
+	p.content_rating,
+	a.primary_genre,
+	CAST(REPLACE(p.install_count, '+','') AS numeric) AS install_count_num,
+	p.type
+from app_store_apps as a
+inner join play_store_apps as p
+ON a.name = p.name
+WHERE  p.install_count >= 1000000 
+	a.price < '1' 
+	AND p.price < '1'
+	AND a.rating BETWEEN 4.5 AND 5.0
+	AND p.rating BETWEEN 4.5 AND 5.0
+GROUP BY
+	a.name,
+	a.price,
+	a.review_count,
+	a.rating,
+	p.rating,
+	a.content_rating,
+	p.content_rating,
+	a.primary_genre,
+	p.install_count,
+	p.type
+ORDER BY 
+	p_rating DESC,
+	a_rating DESC,
+	install_count_num DESC;
+-- What we decided to go with
+select 
+	a.name, 
+	CAST(a.price AS money),
+	CAST(a.rating AS numeric) AS a_rating,
+	CAST(p.rating AS numeric) AS p_rating,
+	CAST(REPLACE(a.content_rating, '+','') AS numeric) AS content_rating_num,
+	p.content_rating,
+	a.primary_genre,
+	CAST(REPLACE(REPLACE(p.install_count, '+',''), ',','') AS numeric) AS install_count_num,
+	p.type
+from app_store_apps as a
+inner join play_store_apps as p
+ON a.name = p.name
+WHERE 
+	a.price < '1' 
+	AND p.price < '1'
+	AND a.rating BETWEEN 4.5 AND 5.0
+	AND p.rating BETWEEN 4.5 AND 5.0
+GROUP BY
+	a.name,
+	a.price,
+	a.review_count,
+	a.rating,
+	p.rating,
+	a.content_rating,
+	p.content_rating,
+	a.primary_genre,
+	p.install_count,
+	p.type
+ORDER BY 
+	p_rating DESC,
+	a_rating DESC,
+	install_count_num DESC;
 
-
+	
