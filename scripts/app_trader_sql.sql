98df2b8bbfa7d8e75d2ca5e9ce42a76684d23ceb
Nathan Martin <nathanmartin5937@gmail.com>
2022-02-24 20:55:18 -0600

    still pushing on

--- scripts/app_trader_sql.sql
+++ scripts/app_trader_sql.sql
@@ -1,22 +1,36 @@
 select 
 	a.name, 
-	CAST(a.price AS money) AS a_price,
-	CAST(p.price AS money) AS p_price,
-	CAST(a.review_count AS numeric) AS a_review_count_num, 
-	CAST(p.review_count AS numeric) AS p_review_count_num,
-	CAST(a.rating AS numeric) AS rating_num,
-	p.rating,
-	CAST(REPLACE(a.content_rating, '+','') AS numeric) AS content_rating_num, 
-	a.primary_genre, 
+	CAST(a.price AS money),
+	CAST(a.rating AS numeric) AS a_rating,
+	CAST(p.rating AS numeric) AS p_rating,
+	CAST(REPLACE(a.content_rating, '+','') AS numeric) AS content_rating_num,
+	p.content_rating,
+	a.primary_genre,
 	CAST(REPLACE(REPLACE(p.install_count, '+',''), ',','') AS numeric) AS install_count_num,
 	p.type
 from app_store_apps as a
 inner join play_store_apps as p
 ON a.name = p.name
-WHERE a_price = < 1 AND p_price = < 1
+WHERE 
+	a.price < '1' 
+	AND p.price < '1'
+	AND a.rating BETWEEN 4.5 AND 5.0
+	AND p.rating BETWEEN 4.5 AND 5.0
+GROUP BY
+	a.name,
+	a.price,
+	a.review_count,
+	a.rating,
+	p.rating,
+	a.content_rating,
+	p.content_rating,
+	a.primary_genre,
+	p.install_count,
+	p.type
 ORDER BY 
-	rating_num DESC,
-	install_count_num DESC,
-	a_review_count_num DESC,  
-	a.price;
+	p_rating DESC,
+	a_rating DESC,
+	install_count_num DESC;
+
+
 
