9031d70f7db3fbe1df8fd6fdf8266a0860a3fc60
mrmoya413 <mr.moya83@gmail.com>
2022-02-26 13:55:16 -0600

    finished sql query

--- Scripts/app_trader_SQL.sql
+++ Scripts/app_trader_SQL.sql
@@ -20,8 +20,6 @@ from play_store_apps;*/
 --save information.  download button on the top right corner.  lets you save results to file.  --
 --git checkout -b RudyMoya    this is how you create a branch--
 
-----------
-
 select 
 	a.name, 
 	CAST(a.price AS money),
@@ -57,10 +55,77 @@ ORDER BY
 	install_count_num DESC;
 
 
+/*  if you already have answers to questions - think about what else (data prespective) what else might be interesting for them?
+example: how much will each app make in it's lifetime.   
+these 10 apps.  this is how much this app will make based off these projections you gave us.
+--she made a query to determine exactly how many apps were completely left off once i joined them all together.  she made a subquery in select and from.--
+
+do whatever you want in the project.*/ 
 
 
+--Rudy M. Price differences by by store--
 
-
-
-
+Select 
+	a.primary_genre, 
+	count(a.primary_genre), 
+	max(a.price) as max_apple,
+	min(a.price) as min_apple,
+	avg(a.price) as avg_apple,
+	max(cast(p.price as money)) as max_google,
+	min(cast(p.price as money)) as min_google,
+		max(cast(a.price as money)) - max(cast(p.price as money)) AS max_diff,
+		min(cast(a.price as money)) - min(cast(p.price as money)) AS min_diff
+FROM 
+	app_store_apps as a
+INNER JOIN 
+	play_store_apps as p
+ON
+	a.name = p.name
+GROUP BY
+	a.primary_genre
+ORDER BY
+	max_apple DESC, 
+	max_google DESC,
+	min_apple DESC,
+	min_google DESC;
 	
+
+--Breakdown by name and price difference--
+
+SELECT a.name, p.name, max(a.price) as max_apple, max(cast(p.price as money)) as max_google
+FROM 
+	app_store_apps as a
+INNER JOIN 
+	play_store_apps as p
+ON
+	a.name = p.name
+WHERE 
+	a.primary_genre = 'Medical'
+GROUP BY
+	a.name,
+	p.name
+ORDER BY
+	max_apple DESC,
+	max_google DESC;
+
+
+--katie's sql statement--
+
+SELECT 
+	a.name,
+	(2*p.rating + 1) AS longevity,
+	((2500 - 1000)*(2*p.rating+1)*12) AS expected_revenue,
+	CASE WHEN a.price <= 1.00 THEN 10000
+		 WHEN a.price > 1.00 THEN a.price*10000 END AS purchase_price,
+	((2500 - 1000)*(2*p.rating+1)*12) - (CASE WHEN a.price <= 1.00 THEN 10000
+		 WHEN a.price > 1.00 THEN a.price*10000 END) AS net
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+GROUP BY 
+	a.name,
+	expected_revenue,
+	longevity,
+	purchase_price
+ORDER BY
+	net DESC;
